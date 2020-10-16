43feb39b9255f320acb21a4f7a44881d3508d463
DiogoBotton <diogo.luo@gmail.com>
2020-10-16 19:43:28 -0300

    adc renderizacao de lista

--- Frontend - react/AulaReact/aula-filmes/src/pages/cadastro/index.tsx
+++ Frontend - react/AulaReact/aula-filmes/src/pages/cadastro/index.tsx
@@ -10,15 +10,15 @@ function Cadastro() {
       <Header description="Faça o Cadastro para o acesso" />
 
       <div className="centro">
-        <div className="login">
+        <form className="login">
           <h1>Login</h1>
 
           <Input type="text" label="Nome" name="nome" />
           <Input type="text" label="Email" name="email" />
           <Input type="password" label="Senha" name="senha" />
 
-          <Button name="Enviar"/>
-        </div>
+          <Button name="Enviar" type="submit"/>
+        </form>
       </div>
 
       <Footer />
--- Frontend - react/AulaReact/aula-filmes/src/pages/filmes/filmes.tsx
+++ Frontend - react/AulaReact/aula-filmes/src/pages/filmes/filmes.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from '../../components/header/index';
 import Footer from '../../components/footer/index';
 import api from '../../services/services';
+import Generos from '../generos/generos';
 
 const Filmes = () => {
     // Explicação: 1° paramêtro se refere à VARIAVEL que será armazenado algum dado, e o 2° paramêtro ao MÉTODO que ATUALIZARÁ o dado.
@@ -15,6 +16,14 @@ const Filmes = () => {
     // Variavel com string de erro, inicia com string vazia
     const [erro, updateErro] = useState('');
 
+    const init = {
+        headers: {
+            // Acrescenta o authorization (token) para ser autenticado na API
+            Authorization: 'Bearer ' + localStorage.getItem('token-usuario'),
+            'Content-Type': 'application/json'
+        },
+    }
+
     // O useEffect é parecido com o componentDidMount (executa após a renderização do componente/página), e outros...
     // 3 fases: Quando monta, quando altera, e quando desfaz
 
@@ -23,7 +32,7 @@ const Filmes = () => {
     // com [conteudo] significa que irá ser executado sempre quando a variavel "conteúdo" houver uma alteração
     useEffect(() => {
         api
-            .get('filmes')
+            .get('filmes', init)
             .then(resp => {
                 // Caso não dê erro, atualiza lista de filmes
                 updateFilmes(resp.data);
@@ -36,12 +45,43 @@ const Filmes = () => {
             })
     }, [])
 
+    // Renderiza todos os filmes
+    const filmesRender = () => {
+        return (
+            <tbody>
+                {filmes.map((filme: any) => {
+                    return (
+                        <tr key={filme.idFilme}>
+                            <td>{filme.idFilme}</td>
+                            <td>{filme.titulo}</td>
+                            <td>{filme.genero.nome}</td>
+                            <td></td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        );
+    }
+
     // ERRO: Executa o useEffect 4 vezes ou mais
     console.log(filmes)
     return (
         <div>
             <Header description="Filmes da plataforma" />
             <h1>{erro}</h1>
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#Id</th>
+                            <th>Nome</th>
+                            <th>Gênero</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                        {filmesRender()}
+                </table>
+            </div>
             <Footer />
         </div>
     );
